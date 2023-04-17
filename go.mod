c80e54a46bd0db6c2c28e778ea673c08eb2c5107
jinyf <jyf0231@163.com>
2023-04-17 16:55:30 +0800

    up singer

--- go.mod
+++ go.mod
@@ -1,3 +1,9 @@
 module github.com/kekek/sign
 
 go 1.18
+
+require (
+	github.com/bwmarrin/go-alone v0.0.0-20190806015146-742bb55d1631 // indirect
+	golang.org/x/crypto v0.8.0 // indirect
+	golang.org/x/sys v0.7.0 // indirect
+)
--- sign.go
+++ sign.go
@@ -1,84 +1,104 @@
-package sign
+package signer
+
+// docs : https://github.com/tsawler/signer
 
 import (
 	"fmt"
-	"strconv"
+	"net/url"
+	"strings"
 	"time"
+
+	goalone "github.com/bwmarrin/go-alone"
 )
 
-var defaultSignKeyList = []string{"id", "timestamp", "ts", "age"}
+const defaultSecret = "6nKcIGq19kxXJYOR"
 
+var SignInstance *Signature
+
+func InitSign(secret string) {
+
+	s := defaultSecret
+	if len(secret) > 0 {
+		s = secret
+	}
+
+	SignInstance = &Signature{
+		Secret: s,
+	}
+}
+
+// Signature is the type for the package. Secret is the signer secret, a lengthy
+// and hard to guess string we use to sign things. The secret must not exceed 64 characters.
 type Signature struct {
-	Secret           string   // 签名密码
-	SignParamKeyName string   // 签名key
-	SignParamKeyList []string // 参与签名的参数名称
-	SignFunc         SignFunc // 签名函数
+	Secret string
 }
 
-var defaultSignature = NewSignature("123456")
-
-func NewSignature(secret string, opts ...SignatureOption) *Signature {
-	s := &Signature{
-		Secret:           secret,
-		SignParamKeyName: "sign",
-		SignParamKeyList: defaultSignKeyList,
-		SignFunc:         Md5,
-	}
-
-	for _, opt := range opts {
-		opt(s)
-	}
-
-	return s
-}
-
-// 加签
+// SignURL generates a signed url and returns it, stripping off http:// and https://
 func (s *Signature) SignURL(data string) (string, error) {
-
-	u, err := NewUrl(data, SetSignKeyName(s.SignParamKeyName), SetSignParamsNameList(s.SignParamKeyList))
+	// verify this is a url
+	_, err := url.ParseRequestURI(data)
 	if err != nil {
 		return "", err
 	}
 
-	toSignStr := u.GetToSignStr()
+	var urlToSign string
 
-	sign := s.SignFunc(toSignStr, s.Secret)
+	ex := strings.Split(data, "//")
+	exploded := strings.Split(ex[1], "/")
+	domain := exploded[0]
+	exploded[0] = ""
+	stringToSign := strings.Join(exploded, "/")
 
-	return u.GetSignedUrl(sign), nil
+	pen := goalone.New([]byte(s.Secret), goalone.Timestamp)
+
+	if strings.Contains(stringToSign, "?") {
+		// handle case where URL contains query parameters
+		urlToSign = fmt.Sprintf("%s&hash=", stringToSign)
+	} else {
+		// no query parameters
+		urlToSign = fmt.Sprintf("%s?hash=", stringToSign)
+	}
+
+	tokenBytes := pen.Sign([]byte(urlToSign))
+	token := string(tokenBytes)
+
+	return fmt.Sprintf("%s//%s%s", ex[0], domain, token), nil
 }
 
-// 验证签名
+// VerifyURL verifies a signed url and returns true if it is valid,
+// false if it is not. Note that http:// and https:// are stripped off
+// before verification
 func (s *Signature) VerifyURL(data string) (bool, error) {
-	u, err := NewUrl(data, SetSignKeyName(s.SignParamKeyName), SetSignParamsNameList(s.SignParamKeyList))
+	_, err := url.ParseRequestURI(data)
 	if err != nil {
 		return false, err
 	}
+	ex := strings.Split(data, "//")
+	exploded := strings.Split(ex[1], "/")
+	exploded[0] = ""
+	stringToVerify := strings.Join(exploded, "/")
 
-	toSignStr := u.GetToSignStr()
+	pen := goalone.New([]byte(s.Secret), goalone.Timestamp)
 
-	sign := s.SignFunc(toSignStr, s.Secret)
+	_, err = pen.Unsign([]byte(stringToVerify))
+	if err != nil {
+		// signature is not valid. Token was tampered with, forged, or maybe it's
+		// not even a token at all! Either way, it's not safe to use it.
+		return false, err
+	}
 
-	oldSign := u.GetSign()
+	// valid hash
+	return true, nil
 
-	fmt.Println("sign", sign, oldSign)
-	return sign == oldSign, nil
 }
 
-func (s *Signature) Expired(data string, minutesUntilExpire int) (bool, error) {
+// Expired checks to see if a token has expired. It returns true if
+// the token was created within minutesUntilExpire, and false otherwise.
+func (s *Signature) Expired(data string, minutesUntilExpire int) bool {
+	exploded := strings.Split(data, "//")
 
-	u, err := NewUrl(data)
-	if err != nil {
-		return true, err
-	}
+	pen := goalone.New([]byte(s.Secret), goalone.Timestamp)
+	ts := pen.Parse([]byte(exploded[1]))
 
-	ts := u.GetTimestamp()
-
-	tsInt64, err := strconv.ParseInt(ts, 10, 64)
-	if err != nil {
-		return true, nil
-	}
-
-	start := time.Unix(tsInt64, 0)
-
-	return time.Since(start) > time.Duration(minutesUntilExpire)*time.Minute, nil
+	return time.Since(ts.Timestamp) > time.Duration(minutesUntilExpire)*time.Minute
 }
